c7aa300121ab723c73ebb5eb7124490755b58b27
cxrlos <carlos.garcia@nubank.com.br>
2026-02-24 15:53:39 -0600

    Fuzzy laxy fix

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -22,29 +22,12 @@ return {
     { "<leader>fd", desc = "Diagnostics" },
     { "<leader>fc", desc = "Command history" },
   },
-  init = function()
-    local map = require("config.map")
-    local d = { group = "Find", docs = "navigation-cheatsheet.md" }
-    local function tb(name)
-      return function()
-        require("telescope.builtin")[name]()
-      end
-    end
-
-    map("n", "<leader>ff", tb("find_files"), vim.tbl_extend("force", d, { desc = "Find files" }))
-    map("n", "<leader>fg", tb("live_grep"), vim.tbl_extend("force", d, { desc = "Live grep" }))
-    map("n", "<leader>fr", tb("oldfiles"), vim.tbl_extend("force", d, { desc = "Recent files" }))
-    map("n", "<leader>fb", tb("buffers"), vim.tbl_extend("force", d, { desc = "Buffers" }))
-    map("n", "<leader>fh", tb("help_tags"), vim.tbl_extend("force", d, { desc = "Help tags" }))
-    map("n", "<leader>fs", tb("lsp_document_symbols"), vim.tbl_extend("force", d, { desc = "Document symbols" }))
-    map("n", "<leader>fw", tb("lsp_workspace_symbols"), vim.tbl_extend("force", d, { desc = "Workspace symbols" }))
-    map("n", "<leader>fd", tb("diagnostics"), vim.tbl_extend("force", d, { desc = "Diagnostics" }))
-    map("n", "<leader>fc", tb("command_history"), vim.tbl_extend("force", d, { desc = "Command history" }))
-  end,
   config = function()
+    local map = require("config.map")
     local shared = require("config.shared")
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
     telescope.setup({
       defaults = {
@@ -70,5 +53,19 @@ return {
     })
 
     pcall(telescope.load_extension, "fzf")
+
+    local d = { group = "Find", docs = "navigation-cheatsheet.md" }
+
+    map("n", "<leader>ff", builtin.find_files, vim.tbl_extend("force", d, { desc = "Find files" }))
+    map("n", "<leader>fg", function()
+      builtin.grep_string({ search = "", only_sort_text = true })
+    end, vim.tbl_extend("force", d, { desc = "Live grep" }))
+    map("n", "<leader>fr", builtin.oldfiles, vim.tbl_extend("force", d, { desc = "Recent files" }))
+    map("n", "<leader>fb", builtin.buffers, vim.tbl_extend("force", d, { desc = "Buffers" }))
+    map("n", "<leader>fh", builtin.help_tags, vim.tbl_extend("force", d, { desc = "Help tags" }))
+    map("n", "<leader>fs", builtin.lsp_document_symbols, vim.tbl_extend("force", d, { desc = "Document symbols" }))
+    map("n", "<leader>fw", builtin.lsp_workspace_symbols, vim.tbl_extend("force", d, { desc = "Workspace symbols" }))
+    map("n", "<leader>fd", builtin.diagnostics, vim.tbl_extend("force", d, { desc = "Diagnostics" }))
+    map("n", "<leader>fc", builtin.command_history, vim.tbl_extend("force", d, { desc = "Command history" }))
   end,
 }
